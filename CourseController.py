cdab6b74dd293d49329cf9509dd816d6bf16bbd1
Nikhil Giridharan <134903029+nikhilgiridharan@users.noreply.github.com>
2024-02-15 21:59:16 -0600

    Add files via upload

--- CourseController.py
+++ CourseController.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 
 # MySQL database configuration
 db_config = {
-    "host": "localhost",
-    "user": "****",
-    "password": "**********",
+    "host": "",
+    "user": "",
+    "password": "",
     "database": "coursecatalog"
 }
+subCategories = {}
+courses = {}
+
 
 @app.route("/")
 def entry():
@@ -21,40 +24,68 @@ def entry():
     cursor.execute(query)
 
     # Fetch all rows
-    rows = cursor.fetchall()
+    global subCategories
+    subCategories= cursor.fetchall()
 
     # Close the cursor and connection
     cursor.close()
     connection.close()
 
-    return render_template("courseSelection.html", subjects=rows)
+    return render_template("courseSelection.html", subjects=subCategories)
 
 
-@app.route("/search", methods=['POST'])
+@app.route("/search", methods=['POST', 'GET'])
 def search():
-    searchtext = request.form['courseSearch']
-    # Create a connection
-    connection = mysql.connector.connect(**db_config)
+    global courses
+    if request.method == 'GET':
+        sortOption = request.args.get('sort')
+        if sortOption == 'difficultyHighLow':
+            sortedCourses = sorted(courses, key=lambda x: x[3], reverse=True)
+        elif sortOption == 'difficultyLowHigh':
+            sortedCourses = sorted(courses, key=lambda x: x[3])
+        elif sortOption == 'teacherQualityHighLow':
+            sortedCourses = sorted(courses, key=lambda x: x[4], reverse=True)
+        elif sortOption == 'teacherQualityLowHigh':
+            sortedCourses = sorted(courses, key=lambda x: x[4])
+        else:
+            # Default sorting or no sorting
+            sortedCourses = courses
+    else:
+        args = {}
+        searchSubject = request.form['subjectCategory']
+        searchText = request.form['searchText']
+        # Create a connection
+        connection = mysql.connector.connect(**db_config)
 
-    # Create a cursor
-    cursor = connection.cursor()
+        # Create a cursor
+        cursor = connection.cursor()
 
-    # Execute a SELECT query
-    query = "SELECT * FROM courses WHERE courseSubject = %s"
-    args = searchtext,
-    cursor.execute(query, args)
+        # Execute a SELECT query
+        query = ("SELECT courses.courseId, courses.CourseName, teachers.TeacherName, " +
+                 "teachers.difficulty, teachers.teacherQuality " +
+                 "FROM courses INNER JOIN teachers ON courses.courseId = teachers.courseId ")
 
-    # Fetch all rows
-    rows = cursor.fetchall()
+        if searchSubject != "":
+            query = query + "AND courses.courseSubject = %(subject)s"
+            args['subject'] = searchSubject
+        if searchText != "":
+            query = query + "AND (courses.courseName like %(text)s  OR courses.courseId like %(text)s)"
+            args['text'] = "%" + searchText + "%"
 
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+        cursor.execute(query, args)
 
-    for row in rows:
-        print(row)
+        # Fetch all rows
+        courses = cursor.fetchall()
 
-    return render_template("searchResults.html", courses=rows)
+        cursor.close()
+        connection.close()
+
+        for row in courses:
+            print(row)
+        sortedCourses = courses
+    global subCategories
+    return render_template("courseSelection.html", courses=sortedCourses,  subjects=subCategories)
+
 
 @app.route("/teacherSearch", methods=['GET'])
 def searchTwo():
